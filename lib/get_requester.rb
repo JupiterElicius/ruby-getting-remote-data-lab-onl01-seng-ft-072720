68dd1396685cfb9c1cda61253743dd191cd920c3
Nick Bazaldua <bazalnick@gmail.com>
2020-08-24 09:17:16 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,6 +7,10 @@ class GetRequester
   URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
   
   def get_response_body
-    
+    uri = URI.parse(url)
+    response.body
   end
+  
+  def oarse_json
+    
 end
