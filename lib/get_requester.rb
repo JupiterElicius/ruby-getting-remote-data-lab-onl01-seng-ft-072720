4abc42e1819877cde2efb1f5cc6bdb5ad2204fad
Nick Bazaldua <bazalnick@gmail.com>
2020-08-24 09:30:22 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,12 +7,13 @@ class GetRequester
   attr_accessor :url
   
   def initialize(url)
-    @url = "#{urlstring}"
+    @url = url
   end
   
   def get_response_body
-    uri = URI.parse(url)
+    uri = URI.parse(@url)
    response = Net::HTTP.get_response(uri)
+   response.body
   end
   
   def parse_json
